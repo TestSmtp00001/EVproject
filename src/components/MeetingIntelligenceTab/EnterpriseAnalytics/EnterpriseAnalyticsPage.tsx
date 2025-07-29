6a381f106150f866261ca1212886cc87374a2775
TestSmtp00001 <testsmtp0107@gmail.com>
2025-07-29 16:57:53 +1000

    Updated EnterpriseAnalyticsPage.tsx

--- src/components/MeetingIntelligenceTab/EnterpriseAnalytics/EnterpriseAnalyticsPage.tsx
+++ src/components/MeetingIntelligenceTab/EnterpriseAnalytics/EnterpriseAnalyticsPage.tsx
@@ -26,10 +26,10 @@ const EnterpriseAnalyticsPage: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white p-6">
+    <div className="min-h-screen bg-white p-3 sm:p-4 md:p-6">
       <div className="max-w-7xl mx-auto">
         
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 gap-4 sm:gap-6">
           <SalesMethodologyCard onDataClick={handleDataClick} />
           <BuyerJourneyCard onDataClick={handleDataClick} />
           <BuyerObjectionsCard onDataClick={handleDataClick} />
